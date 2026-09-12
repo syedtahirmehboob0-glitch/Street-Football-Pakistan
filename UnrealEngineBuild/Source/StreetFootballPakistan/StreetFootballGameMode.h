51732dd15837425b67277aaaf07dfdcff6c0d7b7
syedtahirmehboob0-glitch <syedtahirmehboob0@gmail.com>
2026-09-12 20:15:09 +0300

    Add football game mode foundation

---
+++ UnrealEngineBuild/Source/StreetFootballPakistan/StreetFootballGameMode.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "StreetFootballGameMode.generated.h"
+
+UCLASS()
+class STREETFOOTBALLPAKISTAN_API AStreetFootballGameMode : public AGameModeBase
+{
+    GENERATED_BODY()
+
+public:
+    AStreetFootballGameMode();
+};
